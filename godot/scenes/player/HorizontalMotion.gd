ec46c98ee9b979a44cc135246a084207cbcd07f0
agodot <zoldy.wf@gmail.com>
2018-09-10 10:59:54 -0600

    Added some simple platformer mechanics (walk, jump).

---
+++ godot/scenes/player/HorizontalMotion.gd
@@ -0,0 +1,50 @@
+extends Node
+
+var player = null
+var stateManager = null
+var motion = Vector2(0,0)
+const UP = Vector2(0,-1)
+
+#------------------------------------------------------------
+# At the moment I'm not differentiating between the player
+# being on the ground or in the air, but can add if desired.
+#-------------------------------------------------------------
+var friction_accel = 2500	# How quickly the player slows down.
+var ground_accel = 5000		# How quickly the player can accelerate on the ground.
+var max_ground_vel = 1000	# How quickly the player can move on the ground.
+
+var last_hit = "NEITHER"
+
+#---------------------------------------------------------------------------------------------------
+# I could use if (left_hit)...else(right_hit) to control motion, but if you hit both you just stop.
+# This is maybe OK in the case where the player intentionally hits both, but if they're rapidly
+# switching from going left to going right, there's the appearance of latency.
+#----------------------------------------------------------------------------------------------------
+func _input(event):
+	if Input.is_action_just_pressed("ui_right"): last_hit = "RIGHT"
+	if Input.is_action_just_pressed("ui_left"): last_hit = "LEFT"
+	
+func _physics_process(delta):
+	
+	if self.stateManager == null: self.stateManager = get_node("../StateManager")
+	if self.player == null: self.player = get_parent()
+	self.player.move_and_slide(	motion, UP )
+	
+	# Move left.
+	if Input.is_action_pressed("ui_left") and not (Input.is_action_pressed("ui_right") and last_hit == "RIGHT"):
+		if (self.motion.x > -max_ground_vel):
+			self.motion.x -= ground_accel * delta
+		else: self.motion.x = -max_ground_vel
+	# Move right.
+	if Input.is_action_pressed("ui_right") and not (Input.is_action_pressed("ui_left") and last_hit == "LEFT"):
+		if (self.motion.x < max_ground_vel):
+			self.motion.x += ground_accel * delta
+		else: self.motion.x = max_ground_vel
+
+	# Enact friction.
+	if self.motion.x > friction_accel * delta:
+		self.motion.x -= friction_accel * delta
+	elif self.motion.x < -friction_accel * delta:
+		self.motion.x += friction_accel * delta
+	else:
+		self.motion.x = 0
---
+++ godot/scenes/player/VerticalMotion.gd
@@ -0,0 +1,52 @@
+extends Node
+
+var jumps = 0
+var max_jumps = 2
+var jump_velocity = 1200
+var gravity_accel = 3000
+
+var motion = Vector2(0,0)
+const UP = Vector2(0,-1)
+
+var player = null
+var stateManager = null
+var jump_pressed = false
+
+func _physics_process(delta):
+	
+	if self.stateManager == null: self.stateManager = get_node("../StateManager")
+	if self.player == null: self.player = get_parent()
+
+	self.player.move_and_slide(	motion, UP )
+	
+	# Quadratic function has a linear derivative.
+	self.motion.y += delta * self.gravity_accel 
+
+	# Landing on the floor resets the player's
+	# jump counter, and their downwards velocity.
+	if self.player.is_on_floor():
+		self.motion.y = 0
+		self.jumps = 0
+		self.jump_pressed = false # This is so the player can 'bounce'.
+
+	# -----------------------------------------------------------------
+	# You *cannot* just use Input.is_action_just_pressed("ui_up") here.
+	# Because we're calling in _process rather than _input, just_pressed
+	# can be evaluated true more than once for a single press, which
+	# results in the double jump being eaten on the first jump.
+	#
+	# I avoid using _input here because if I did it would make 'bouncing'
+	# (see line 26) more tedious to implement.
+	# ------------------------------------------------------------------
+	if Input.is_action_pressed("ui_up"):
+		
+		if not self.jump_pressed:
+			
+			if self.jumps < self.max_jumps:
+				
+				self.jump_pressed = true
+				self.motion.y = -self.jump_velocity
+				self.jumps += 1
+				
+	else:
+		self.jump_pressed = false
